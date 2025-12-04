3ac0b0e2b3ee93efefba14f0ff8e1a0288f01c2d
Aritra-kayal <aritra9434853448@gmail.com>
2025-12-04 19:58:04 +0530

    new code

---
+++ newLine2.java
@@ -0,0 +1,8 @@
+public class newLine2 {
+    public static void main(String args[]) {
+        System.out.print("Hello Aritra\n");
+        System.out.print("Hello Aritra\n");
+        System.out.println("Hello Aritra");
+        System.out.println("Hello Aritra");
+    }
+}
