75516dfcc8cc0478f7cc58c5239a510335326cab
Aritra-kayal <aritra9434853448@gmail.com>
2025-12-05 23:23:19 +0530

    new code

---
+++ variable3.java
@@ -0,0 +1,14 @@
+public class variable3 {
+    public static void main(String srgs[]) {
+        int a = 10;
+        System.out.println(a);
+        char ch = '@';
+        System.out.println(ch);
+        float b = 3.14f;
+        System.out.println(b);
+        String name = "Aritra Kayal";
+        System.out.println(name);
+        a = 20;
+        System.out.println(a);
+    }
+}
