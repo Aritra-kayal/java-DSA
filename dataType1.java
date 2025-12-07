650e5b82cc1c1d0aa9b15a8c21a530317bc0148e
Aritra-kayal <aritra9434853448@gmail.com>
2025-12-07 19:18:30 +0530

    new code

---
+++ dataType1.java
@@ -0,0 +1,20 @@
+public class dataType1 {
+    public static void main(String args[]) {
+        int a = 10;
+        System.out.println(a);
+        float b = 3.14f;
+        System.out.println(b);
+        char ch = '*';
+        System.out.println(ch);
+        byte by = -128;
+        System.out.println(by);
+        short s = 350;
+        System.out.println(s);
+        boolean bu = true;
+        System.out.println(bu);
+        long big = 12345678;
+        System.out.println(big);
+        double du = 35.93783;
+        System.out.println(du);
+    }
+}
---
+++ sumOfTwoNumbers2.java
@@ -0,0 +1,10 @@
+// sum of two numbers
+/*this is my 1st sum code*/
+public class sumOfTwoNumbers2 {
+    public static void main(String args[]) {
+        int a = 10, b = 20;
+        int sum;
+        sum = a + b;
+        System.out.println("Sum of two number is: " + sum);
+    }
+}
