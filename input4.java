4d33c28391b7ce0673bcf4b9dc6d83ef9e541e91
Aritra-kayal <aritra9434853448@gmail.com>
2025-12-08 13:48:16 +0530

    new code

---
+++ input4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class input4 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("int number is : " + a);
+        float b = sc.nextFloat();
+        System.out.println("float value is : " + b);
+        boolean c = sc.nextBoolean();
+        System.out.println("it is boolean value: " + c);
+        byte by = sc.nextByte();
+        System.out.println("this is byte value: " + by);
+        short sh = sc.nextShort();
+        System.out.println("thid is short value:" + sh);
+        long lo = sc.nextLong();
+        System.out.println("This is a long value: " + lo);
+        double d = sc.nextDouble();
+        System.out.println("This is a double value: " + d);
+    }
+}
