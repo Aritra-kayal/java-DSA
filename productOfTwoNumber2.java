a05a809644764e386fa346be6fd113f6748f8b6a
Aritra-kayal <aritra9434853448@gmail.com>
2025-12-12 19:54:39 +0530

    new code

---
+++ productOfTwoNumber2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class productOfTwoNumber2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Eneter 1st number: ");
+        int a = sc.nextInt();
+        System.out.print("Eneter 2nd number: ");
+        int b = sc.nextInt();
+        int m = a * b;
+        System.out.println("Product of Two numbers is: " + m);
+    }
+}
