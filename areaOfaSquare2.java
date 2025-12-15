8ae4f7397d619ef98af60526f578384a8db25ae4
Aritra-kayal <aritra9434853448@gmail.com>
2025-12-15 20:20:40 +0530

    new code

---
+++ areaOfaSquare2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class areaOfaSquare2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Eneter the value of side: ");
+        float s = sc.nextFloat();
+        float area = s * s;
+        System.out.println("Area of a square is: " + area);
+    }
+}
