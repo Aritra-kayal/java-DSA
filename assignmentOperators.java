4640f2490622cae6e6d0dda8027a664cb28b6ecf
Aritra-kayal <aritra9434853448@gmail.com>
2025-12-20 19:08:24 +0530

    assignment operator

---
+++ assignmentOperators.java
@@ -0,0 +1,11 @@
+public class assignmentOperators {
+    public static void main(String args[]) {
+        int b = 10, a;
+        a = b;
+        System.out.println(a += 1);
+        System.out.println(a -= 1);
+        System.out.println(a *= 1);
+        System.out.println(a /= 1);
+        System.out.println(a %= 1);
+    }
+}
